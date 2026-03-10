9f919542d486f77083cb147b454d6cb9bc28c8c1
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-03-10 17:11:04 +0900

    ランダムコード生成関数を追加

---
+++ fuzz/src/generate.rs
@@ -0,0 +1,52 @@
+use std::ops::Range;
+
+use rand::{RngExt, rngs::ThreadRng};
+
+pub fn generate_random_program(rng: &mut ThreadRng, size_range: &Range<usize>) -> String {
+    let mut code = String::new();
+    let mut level: i32 = 0;
+
+    loop {
+        if code.len() >= size_range.end {
+            code.clear();
+            level = 0;
+        }
+        match rng.random_range(0u8..8u8) {
+            0 => {
+                code.push_str("+");
+            }
+            1 => {
+                code.push_str("-");
+            }
+            2 => {
+                code.push_str("<");
+            }
+            3 => {
+                code.push_str(">");
+            }
+            4 => {
+                code.push_str("[");
+                level += 1;
+            }
+            5 => {
+                level -= 1;
+                if level == -1 {
+                    if size_range.start > code.len() {
+                        level = 0;
+                        continue;
+                    } else {
+                        return code;
+                    }
+                }
+                code.push_str("]");
+            }
+            6 => {
+                code.push_str(".");
+            }
+            7 => {
+                code.push_str(",");
+            }
+            _ => unreachable!(),
+        }
+    }
+}
--- fuzz/src/main.rs
+++ fuzz/src/main.rs
@@ -1,3 +1,12 @@
+use rand::rng;
+
+use crate::generate::generate_random_program;
+
+mod generate;
+
 fn main() {
-    println!("hello");
+    let mut rng = rng();
+    for _ in 0..10000 {
+        let code = generate_random_program(&mut rng, &(100..10000));
+    }
 }
